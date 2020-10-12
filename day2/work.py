84e7a1b22ed21d1a838801668da155b1c7b64286
吴中卉 <wuzhonghui@wuzhonghuideMac-mini.local>
2020-10-12 18:22:44 +0800

    装饰器

--- day2/work.py
+++ day2/work.py
@@ -2,5 +2,21 @@
 # _author_:wuzhonghui
 # 2020/10/12
 
+'''
+global用法：
+    如果需要在函数内部改变函数外部的变量，就可以通过在函数内部声明变量为global变量。这样当程序运行至global变量便会替换外部的同名变量。
+ps：
+    name = '中卉'
+    def test():
+        global name
+        name = 'zhonghui'
+        return name
+
+    print(name)
+    test()
+    pring(name)
+
+执行完后的name就等于 zhonghui 了
+'''
 
 import uuid
